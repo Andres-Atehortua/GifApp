69ad8fdb8e6031587732787341d0d733d21130a3
Andres Atehortua <anderson17colombia@hotmail.com>
2020-07-29 19:37:45 +0200

    docs added

---
+++ docs/precache-manifest.2abf73e66016d7809525f8bf041f259c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "403fc426a7ac27ce4cb2b457a8099a0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5b2971d111b155c9fd8",
+    "url": "/static/css/main.5d00bc8b.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5b2971d111b155c9fd8",
+    "url": "/static/js/main.8c93f7e4.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
